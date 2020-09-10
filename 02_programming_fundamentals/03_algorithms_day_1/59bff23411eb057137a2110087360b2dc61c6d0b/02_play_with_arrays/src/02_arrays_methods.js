f08b9f8cec2f293b51f0f11b599b0c6160020c1d
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-10 17:14:44 +0200

    finish exo 2, 3

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/02_play_with_arrays/src/02_arrays_methods.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/02_play_with_arrays/src/02_arrays_methods.js
@@ -3,9 +3,10 @@
  */
 
 // Rewrite your array `digits` using `push`;
-
+const digits = digits.push ;
 // rewrite your variable `last` using `length`;
-
+const last = last.length ;
 // create another array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
-
+const litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] ;
 // use `join` to create, into the variable `allDigits`, a string like this : `zero - one ...`.
+const allDigits = litteralDigits.join(" - ") ;
