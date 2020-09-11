8e0fe5925201b9b0738c1110b78cec87f989af56
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-11 10:43:00 +0200

    clean before download today ex

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/04_play_with_conditional_statement_exoplanets/src/index.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/04_play_with_conditional_statement_exoplanets/src/index.js
@@ -3,7 +3,26 @@ const star = require("./data/starData");
 const planet = require("./data/exoplanetData");
 
 // Your code here:
-let canHabitateLife;
 
+const exoplanetLifeCondition = {
+mass: 0.77,
+radius: 0.91,
+rotationStability: true,
+habitalZone: true,
+spectralClass: "M",
+temperature: 2550,
+mass: 0.089,
+};
+
+const exoplanet1 = {
+    mass: 0.77,
+    radius: 0.91,
+    rotationStability: true,
+    habitalZone: true,
+    spectralClass: "M",
+    temperature: 2550,
+    mass: 0.089,
+
+    const canHabitateLife = exoplanet1 === exoplanetLifeCondition;
 // Don't modifiy this line for test to work properly.
 module.exports = canHabitateLife;
