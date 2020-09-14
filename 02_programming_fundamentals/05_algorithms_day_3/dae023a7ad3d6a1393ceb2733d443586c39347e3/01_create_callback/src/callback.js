3782094c875f08178c184543e442eaeed90ea8b2
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-14 11:08:52 +0200

    yarn install

---
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/01_create_callback/src/callback.js
@@ -0,0 +1,15 @@
+// code here
+const sayHello = player => console.log(`Hello, ${player.name}`) ;
+// leave the code below for tests to work
+module.exports = sayHello;
+
+
+
+/* 
+the `thisIsThePlayer` function waits for a _callback function_ as a parameter.
+
+In the **`src/callback.js` file**, write a `sayHello` **arrow function**. It should take a `player` as a parameter and greet the player by its name when we execute the code:
+
+```js
+"Hello, Spartacus";
+``` */
