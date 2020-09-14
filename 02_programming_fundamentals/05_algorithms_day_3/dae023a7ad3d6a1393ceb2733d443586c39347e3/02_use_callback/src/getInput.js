951a96050b02268bb7fe7b7f52fe4023115a010d
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-14 11:47:41 +0200

    exo 2 c bon

--- 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/02_use_callback/src/getInput.js
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/02_use_callback/src/getInput.js
@@ -1,6 +1,8 @@
 // A function that takes two parameters, the second one being a callback function
 
-const getInput = // code the function here
+const getInput = (cequetumenvoi, callback) => callback(cequetumenvoi.key);
+
+
 
 // Leave the line below for tests to work
 module.exports = getInput;
