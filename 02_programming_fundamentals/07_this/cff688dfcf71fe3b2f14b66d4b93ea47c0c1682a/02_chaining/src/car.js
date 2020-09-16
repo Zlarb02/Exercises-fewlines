b87c7229a8712cfe01265781ad06f921b437fc7c
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-16 14:45:28 +0200

    exo2 finish

--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/car.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/car.js
@@ -1,5 +1,27 @@
 const car = {
   // Complete here
+  start: function () {
+    this.speed = 0;
+    this.minutes = 0;
+    this.distance = 0;
+
+    return this;
+  },
+  changeSpeed: function (speed) {
+    //console.log(speed + "kmh");
+    this.speed = speed;
+    return this;
+  },
+  drive: function (time) {
+    //console.log(time + "min");
+    this.time = time;
+    this.distance = this.distance + Math.round((this.speed * this.time) / 6) / 10;
+    return this;
+  },
+  showDistance: function () {
+    console.log(this.distance + " Km");
+    return this;
+  },
 };
 
 module.exports = car;
--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/index.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/02_chaining/src/index.js
@@ -1,3 +1,3 @@
 const car = require("./car");
 
-car.start().changeSpeed(130).drive(42).showDistance();
+car.start().changeSpeed(130).drive(45).showDistance().changeSpeed(50).drive(30).showDistance().changeSpeed(90).drive(80).showDistance();
