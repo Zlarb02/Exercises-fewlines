ce63e2251417cb876e74e23ec3bab018c772f63b
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-16 10:25:54 +0200

    finish exo 1

--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/01_using_this/src/person.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/01_using_this/src/person.js
@@ -6,6 +6,10 @@ const person = {
     return `${this.firstname} ${this.lastname}`;
   },
   // Complete here
+  introduceMyself: function () {
+    const intro = "Hello! I'm " + this.firstname + " " + this.lastname + " and I'm " + this.age + ".";
+    return intro;
+  },
 };
 
 module.exports = person;
