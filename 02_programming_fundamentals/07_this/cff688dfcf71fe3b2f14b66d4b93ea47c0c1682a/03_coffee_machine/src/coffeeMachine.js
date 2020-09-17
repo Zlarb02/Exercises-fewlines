7a34d6c215a07c64303419b2546a19fc8c5b0f23
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-17 09:57:20 +0200

    human factory try

--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/03_coffee_machine/src/coffeeMachine.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/03_coffee_machine/src/coffeeMachine.js
@@ -21,29 +21,5 @@ const machine = {
   }
     
 };
-/*
-const machine = {
-  // Complete here
-  litersOfCoffee:0,
-  fillWithLitersOfCoffee: function(litersOfCoffee) {
-    this.litersOfCoffee=litersOfCoffee;
-    return this;
-  },
-  expresso: function() {
-    if (this.litersOfCoffee >= 0.08) {
-      this.litersOfCoffee = this.litersOfCoffee - 0.08;
-      return true;
-    }
-    return false;
-  },
-  longCoffee: function() {
-    if (this.litersOfCoffee >= 0.15) {
-      this.litersOfCoffee=this.litersOfCoffee - 0.15;
-      return true;
-    }
-    return false;
-  },                        
-};*/
-
 
 module.exports = machine;
--- 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/04_factory/src/humanFactory.js
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/04_factory/src/humanFactory.js
@@ -1,6 +1,32 @@
-const humanFactory = () => {};
+const humanFactory = (objethumanidentityaveckeysandvaluecorespondante) => {
+  if (objethumanidentityaveckeysandvaluecorespondante == { genre: "female" }) {
+    let human = objethumanidentityaveckeysandvaluecorespondante;
+    human.firstName = "Jane";
+    human.lastName = "Doe";
+    human.genre = "female";
+    human.job = "unemployed";
+  }
+  function fullname(human) {
+    console.log(`${human.firstname} ${human.lastname}`);
+  }
+  function introduction(human) {
+    console.log("Hello! My name is " + `${human.firstname} ${human.lastname}` + ".");
+    return this;
+  }
+};
 
-const createHumans = () => {};
+const createHumans = (arrayHuman) => {
+  let numberofhuman = 0;
+  const human1 = {
+    ...arrayHuman[numberofhuman],
+  };
+  numberofhuman++;
+  const human2 = {
+    ...arrayHuman[numberofhuman],
+  };
+  const humans = [human1, human2];
+  return humans;
+};
 
 module.exports = {
   humanFactory,
