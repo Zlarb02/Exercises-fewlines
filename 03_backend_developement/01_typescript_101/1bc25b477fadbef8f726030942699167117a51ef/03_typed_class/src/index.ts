1fd7a031fd8672aab353f8059fbe05c4556812d8
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-09-23 16:17:16 +0200

    Finish exo 3

--- 03_backend_developement/01_typescript_101/1bc25b477fadbef8f726030942699167117a51ef/03_typed_class/src/index.ts
+++ 03_backend_developement/01_typescript_101/1bc25b477fadbef8f726030942699167117a51ef/03_typed_class/src/index.ts
@@ -1,6 +1,29 @@
 class Bird {
   // Write your code here.
+  age: number;
+
+  constructor(age: number) {
+    this.age = age;
+  }
+
+  sing(): void {
+    console.log("Cui cui");
+  }
+  fly(time: number): void {
+    let distance: number = 1;
+    if (this.age <= 1) {
+      console.log("The bird is too young to fly");
+    } else if (this.age <= 3) {
+      console.log(`The bird flew ${distance * time}m in ${time}s`);
+    } else if (this.age > 3) {
+      distance = 2;
+      console.log(`The bird flew ${distance * time}m in ${time}s`);
+    }
+  }
 }
+
+const newBird = new Bird(4);
+newBird.fly(1);
 
 // Leave the line below for tests to work properly.
 export { Bird };
