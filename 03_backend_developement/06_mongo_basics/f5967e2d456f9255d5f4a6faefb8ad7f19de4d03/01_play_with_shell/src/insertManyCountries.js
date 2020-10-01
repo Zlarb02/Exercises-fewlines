1ff4c5f66f0c172df48ab14b79f65fcca305f436
Etienne Pogoda <pogoda.etienne@outlook.fr>
2020-10-01 14:40:29 +0200

    finish exo1

--- 03_backend_developement/06_mongo_basics/f5967e2d456f9255d5f4a6faefb8ad7f19de4d03/01_play_with_shell/src/insertManyCountries.js
+++ 03_backend_developement/06_mongo_basics/f5967e2d456f9255d5f4a6faefb8ad7f19de4d03/01_play_with_shell/src/insertManyCountries.js
@@ -1,3 +1,20 @@
 /* global db */
 
 // write your MongoDB shell command here
+db.worldAtlas.insertMany([
+  {
+    name: "France",
+    capital: "Paris",
+    continent: "Europe",
+  },
+  {
+    name: "Iceland",
+    capital: "Icecity",
+    continent: "Icecontinent",
+  },
+  {
+    name: "Salamalec",
+    capital: "Bonjour",
+    continent: "Politesse",
+  },
+]);
--- 03_backend_developement/06_mongo_basics/f5967e2d456f9255d5f4a6faefb8ad7f19de4d03/01_play_with_shell/src/updateOneCountry.js
+++ 03_backend_developement/06_mongo_basics/f5967e2d456f9255d5f4a6faefb8ad7f19de4d03/01_play_with_shell/src/updateOneCountry.js
@@ -1,3 +1,7 @@
 /* global db */
 
 // write your MongoDB shell command here
+db.worldAtlas.updateOne(
+  { name: "Australia" },
+  { $set: { capital: "Canberra" } }
+);
